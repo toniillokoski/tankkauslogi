eda727c7f639af30045113b4340fc8b09fb0ffd1
Toni Illokoski <toni.illokoski@gmail.com>
2020-04-24 21:32:12 +0300

    Luotu testidata.

---
+++ src/testdata.js
@@ -0,0 +1,28 @@
+const testdata = [
+    {   id: "1",
+        asema: 'Neste',
+        litra: '45.54l',
+        euro: '66.44€',
+        kilometrit: '145000',
+        pvm: '2020-04-24' },
+    {   id: "2",
+        asema: 'ABC',
+        litra: '34,29l',
+        euro: '50.00',
+        kilometrit: '146450',
+        pvm: '2020-04-27' },
+    {   id: "3",
+        asema: 'St1',
+        litra: '13.63l',
+        euro: '20.00€',
+        kilometrit: '146987',
+        pvm: '2020-04-30' },
+    {   id: "4",
+        asema: 'Shell',
+        litra: '74.93l',
+        euro: '51.01€',
+        kilometrit: '147002',
+        pvm: '2020-05-01' }
+];
+
+export default testdata;
